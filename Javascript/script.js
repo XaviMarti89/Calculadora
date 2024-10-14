41c5bbda903b05e15e4d7f9f837ea269d61b0ca5
XaviMarti89 <125730044+XaviMarti89@users.noreply.github.com>
2024-10-14 10:40:29 +0200

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+function suma() {
+    let n1 = parseFloat(prompt("Dime un num"));
+    let n2 = parseFloat(prompt("Dime otro num"));
+    document.write(n1+n2)
+}
+function resta() {
+    let n1 = parseFloat(prompt("Dime un num"));
+    let n2 = parseFloat(prompt("Dime otro num"));
+    document.write(n1-n2)
+}
+function multiplicacion() {
+    let n1 = parseFloat(prompt("Dime un num"));
+    let n2 = parseFloat(prompt("Dime otro num"));
+    document.write(n1*n2)
+}
+function division() {
+    let n1 = parseFloat(prompt("Dime un num"));
+    let n2 = parseFloat(prompt("Dime otro num"));
+    document.write(n1/n2)
+}
+
+function arrel() {
+    let n1 = parseFloat(prompt("Dime un num"));
+    document.write(Math.sqrt(n1))
+}
